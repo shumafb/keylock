2169c74d21b9fb71ad09e312a839fe93391a69bd
shumafb <shumafb@gmail.com>
2022-10-30 01:09:05 +0300

    Add remove function

--- db.py
+++ db.py
@@ -2,27 +2,43 @@ from tinydb import TinyDB, Query
 from prettytable import PrettyTable
 
 db = TinyDB('db.json')
-view = PrettyTable()
+Search = Query()
+
 
 def asciiview():
+    view = PrettyTable()
+    """Вывод все эементы БД в табличном виде"""
     view.field_names = ['Название', 'Логин', 'Пароль', 'Примечание']
     for i in db:
         view.add_row(list(i.values()))
     print(view)
 
 def additem(item):
-    Search = Query()
+    """Принимает строку с данными и вносит новый элемент в БД"""
     item = item.split()
     while True:
-        x = input('Хотите добавить заметку? [(Д)а/(Н)ет] ')
-        if x[0].lower() == 'д':
-            notes = input('Введите заметку: ')
+        x = input('Хотите добавить примечание? [(Д)а/(Н)ет][(Y)es/(N)o] ')
+        if x[0].lower() == 'д' or x[0].lower() == 'y':
+            notes = input('Введите текст примечания: ')
+            print('Карточка добавлена')
             break
-        elif x[0].lower() == 'н':
+        elif x[0].lower() == 'н' or x[0].lower() == 'n':
             notes = None
+            print('Карточка добавлена')
             break
         else:
             print('Введите одну из команд')
     db.update({'notes': notes}, Search.name == item[0])
     return db.insert({'name': item[0], 'login': item[1], 'password': item[2], 'notes': notes})
 
+def removeitem(item):
+    """Принимает строку и удаляет элемент с подходящими входными данными"""
+    while True:
+        if Search.name == item:
+            print('Карточка успешно удалена')
+            x = db.get(Search.name == item)
+            return db.remove(doc_ids= [x.doc_id])
+            break
+        else:
+            print('Карточка отсутствует. Проверьте данные')
+            break
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import hello
+import welcome
 import db
 
-hello.hello()
+welcome.hello()
 while True:
-    cmd = input('Введите одну из команд \n(Д)обавить / (П)осмотреть / (В)ыход\n'
-                '(A)dd / (V)iew / (E)xit: ')
+    cmd = input('\nВведите одну из команд \n(Д)обавить / (У)далить / (П)осмотреть / (В)ыход\n'
+                '(A)dd / (R)emove / (V)iew / (E)xit: ')
     if cmd[0].lower() == 'в' or cmd[0].lower() == 'e':
         exit()
     elif cmd[0].lower() == 'д' or cmd[0].lower() == 'a':
         db.additem(input('Введите данные для добавления: [Название] [Логин] [Пароль]: '))
     elif cmd[0].lower() == 'п' or cmd[0].lower() == 'v':
         db.asciiview()
+    elif cmd[0].lower() == 'у' or cmd[0].lower() == 'r':
+        db.removeitem(input('Введите [Название]: '))
     else:
         None
